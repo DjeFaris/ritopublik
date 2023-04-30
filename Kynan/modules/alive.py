d98a9f4c9e0d624226f227a86e8e97ddfd25e816
DjeFaris <89874919+DjeFaris@users.noreply.github.com>
2023-04-30 16:55:50 +0700

    Update alive.py

--- Kynan/modules/alive.py
+++ Kynan/modules/alive.py
@@ -9,17 +9,17 @@ from Kynan.events import register
 from Kynan import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/1898443970930aee25dd4.jpg"
+PHOTO = "https://telegra.ph/file/ac39d453c8f9b93859009.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   TEXT = f"┏━━━━━━━━━━━━━━━━━━━━┓\n"
-  TEXT += f"┠➣ **ꝛɪᴛσ ꝛσʙσᴛ**\n"
-  TEXT += f"┠➣ **ᴍʏ ᴏᴡɴᴇʀ : [ꝛɪᴛσ](https://t.me/kiritonibos)**\n"
+  TEXT += f"┠➣ **ᴍᴀɴᴀɢᴇʀ ʙᴏᴛ**\n"
+  TEXT += f"┠➣ **ᴍʏ ᴏᴡɴᴇʀ : [OWNER](https://t.me/{OWNER_USERNAME)**\n"
   TEXT += f"┠➣ **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`\n"
   TEXT += f"┠➣ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n"
   TEXT += f"┠➣ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n"
   TEXT += f"┠➣ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n"
   TEXT += "┗━━━━━━━━━━━━━━━━━━━━┛"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/rito_robot?start=help"), Button.url("ᴅᴏɴᴀsɪ ​❤️", "https://telegra.ph/file/67d31a4224e3e0211448a.jpg")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", "https://t.me/{BOT_USERNAME}?start=help"), Button.url("ᴅᴏɴᴀsɪ ​❤️", "https://telegra.ph/file/67d31a4224e3e0211448a.jpg")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
